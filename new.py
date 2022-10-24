c0ca70d372575c831a66b23ea84accc3e947f0a0
Ильдар Ибатуллин <inomus332@gmail.com>
2022-10-25 00:32:43 +0300

    Были добавлены Докстринги

--- new.py
+++ new.py
@@ -1,10 +1,12 @@
-"""Импорт позволяет выбить случайные показатели."""
+"""Первый импорт позволяет выбить случайные показатели, а именно урон/защита/выносливость/лечение.
+Второй импорт позволяет запустить анимаю в начале программы."""
 
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
-    """Определяется какой урон получит противник."""
+    """Определяется какой урон получит противник"""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -62,8 +64,9 @@ def start_training(char_name: str, char_class: str) -> str:
     return 'Тренировка окончена.'
 
 
+
 def choice_char_class() -> str:
-    """Описывается выбор и способности героя."""
+    """Описывается выбор героя."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -83,3 +86,17 @@ def choice_char_class() -> str:
                                'или любую другую кнопку, '
                                'чтобы выбрать другого персонажа ').lower()
     return char_class
+
+
+def main() -> None:
+    """Описывается приветственное окно."""
+    run_screensaver()
+print('Приветствую тебя, искатель приключений!')
+print('Прежде чем начать игру...')
+char_name: str = input('...назови себя: ')
+print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+print('Ты можешь выбрать один из трёх путей силы:')
+print('Воитель, Маг, Лекарь')
+char_class: str = choice_char_class()
+print(start_training(char_name, char_class))
